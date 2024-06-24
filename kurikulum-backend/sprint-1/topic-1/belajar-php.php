639aa58c5b49ca7f53d2b0bb9fefd3bf8129c040
Muhammad Nur Ibnu Hubab <nuribnuu@gmail.com>
2024-06-24 16:26:37 +0700

    Menambahkan folder kurikulum-backend

---
+++ kurikulum-backend/sprint-1/topic-1/belajar-php.php
@@ -0,0 +1,24 @@
+<?php
+// Tipe Data
+
+// String
+echo strlen('Hello World'). PHP_EOL;
+
+// Integer
+echo 12 . PHP_EOL;
+
+// Float
+echo 1.2 . PHP_EOL;
+
+// Boolean
+var_dump(false);
+
+// Array
+$dataType = array("String", 1, 1.2, true, null);
+var_dump($dataType);
+var_dump($dataType[3]);
+
+echo "Ini $dataType[0]" . PHP_EOL;
+
+// Null
+var_dump(null) . PHP_EOL;
